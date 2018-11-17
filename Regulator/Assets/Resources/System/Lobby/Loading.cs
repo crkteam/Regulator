eed977fec85977332b2a636ba2e9db836a296b6b
CrkJK <crkjk@CrkJKde-MacBook-Pro.local>
2018-11-17 21:39:50 +0800

    crkJK
    abcdefg

---
+++ Regulator/Assets/Resources/System/Lobby/Loading.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Loading : MonoBehaviour
+{
+	private Image image;
+
+	// Use this for initialization
+	void Start ()
+	{
+		image = GameObject.Find("Scenes").GetComponent<Image>();
+		InvokeRepeating("openScenes",0,0.1f);
+	}
+
+	void openScenes()
+	{
+		//開始漸淡
+		//if(漸淡完){
+		//		GameObject.Find("Main Camera").GetComponent<LobbyController>().enabled = true;
+		//}
+	}
+}
