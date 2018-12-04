fcd0ada3ac034454faaae62baa046db348581573
CrkJK <crkjk@CrkJKde-MacBook-Pro.local>
2018-12-04 11:43:12 +0800

    crk
    jk

---
+++ Regulator/Assets/Test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+	private TextAsset ep1;
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		ep1 = Resources.Load<TextAsset>("ep2");
+		Debug.Log(ep1.text.Split('\n')[1]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
