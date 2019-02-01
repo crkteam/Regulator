0071d16a7ef54f0c49b61d74cf6a72454d1387a1
crkteam <39040121+crkteam@users.noreply.github.com>
2019-02-01 23:27:44 +0800

    crkJK
    abcdefg

---
+++ Regulator/Assets/Resources/System/Open/Script/OpenTitleController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OpenTitleController : MonoBehaviour
+{
+    public FadeController fc;
+
+    public Image BlackBG;
+
+    // Use this for initialization
+    void Start()
+    {
+        // step 1
+        // is Title
+        // when title end execute steop2  
+    }
+
+    public void step2()
+    {
+        InvokeRepeating("check", 2, 0.1f);
+    }
+
+    void check()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            fc.fadein(BlackBG, 0, 0.01f);
+            Invoke("nextStage", 3);
+            CancelInvoke("check");
+        }
+    }
+
+    void nextStage()
+    {
+        SceneManager.LoadScene("KillerScenes");
+    }
+}
---
+++ Regulator/Assets/Resources/System/Open/Script/OpenTitle_Title.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.Remoting.Messaging;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class OpenTitle_Title : MonoBehaviour
+{
+    public Image teamlogo, bg, titlelogo;
+    public Text start;
+
+    public FadeController faceController;
+
+    public OpenTitleController otc;
+    // Use this for initialization
+    void Start()
+    {
+        faceController.OnToWhiteEnd += OnBlackBgToWhite;
+        faceController.OnFadeOutEnd += OnTeamLogoFadeOutEnd;
+        faceController.OnFadeInEnd += OnTeamLogoFadeInEnd;
+        faceController.fadein(teamlogo,0,0.01f); // step1
+    }
+
+    void OnTeamLogoFadeInEnd()
+    {
+        faceController.fadeout(teamlogo,3,0.01f);//step2
+        faceController.OnFadeInEnd -= OnTeamLogoFadeInEnd;
+    }
+
+    void OnTeamLogoFadeOutEnd()
+    {
+        faceController.toWhite(bg,0,0.01f);//step3
+        faceController.OnFadeOutEnd -= OnTeamLogoFadeOutEnd;      
+    }
+
+    void OnBlackBgToWhite()
+    {
+        faceController.fadein(titlelogo,1,0.01f);
+        Invoke("showText",3);
+        faceController.clear();
+        
+    }
+
+    void showText()
+    {
+        start.GetComponent<TW_Regular>().StartTypewriter();
+        otc.step2();
+    }
+
+}
