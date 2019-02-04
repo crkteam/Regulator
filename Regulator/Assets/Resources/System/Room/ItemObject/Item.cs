0ab93a9f86fd2a15492f4cebcc37b723e40937d1
crkteam <39040121+crkteam@users.noreply.github.com>
2019-02-04 13:35:05 +0800

    456
    7/9

---
+++ Regulator/Assets/Resources/System/Room/ItemObject/Item.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour
+{
+    // String
+    public TextAsset item;
+    public String[] content;
+
+    private int count;
+    // objcet
+    private GameObject itemDialogue;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("apple");
+        content = item.text.Split('\n');
+        itemDialogue = GameObject.Find("ItemDialogue");
+        
+        openBlock();
+    }
+
+    void openBlock()
+    {
+        itemDialogue.SetActive(true);
+    }
+    
+    void startDialogue()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
